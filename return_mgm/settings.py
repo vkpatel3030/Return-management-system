51d839cf8a3223da53274ba6f6b81106f8a43eea
vkpatel3030 <vanshupatel3030@gmail.com>
2025-08-05 21:32:47 +0530

    Code commit

--- return_mgm/settings.py
+++ return_mgm/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'return_mgm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/temp/db.sqlite3',
     }
 }
 
