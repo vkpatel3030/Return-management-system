5f55dda0a2eea02b7fdd1271aafb5de4be38b73e
vkpatel3030 <vanshupatel3030@gmail.com>
2025-08-11 19:53:09 +0530

    Code commit

--- return_mgm/urls.py
+++ return_mgm/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('', include('auth_google.urls')),  # ✅ આ line add કરી છે
 ]
- + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
